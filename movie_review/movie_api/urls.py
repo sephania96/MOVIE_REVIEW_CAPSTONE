7bb65c466a87b9d4bfe35f4c671323695ab587a1
Sephania96 <koutanyi@yahoo.com>
2024-10-03 14:12:34 +0100

    update

--- movie_review/movie_api/urls.py
+++ movie_review/movie_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import ListMovie, DetailMovie, MovieViewSet
+from .views import MovieListView, MovieDetailView
+
+#router setup
 router = DefaultRouter()
 router.register(r"movies", MovieViewSet, basename="movies")
 
@@ -9,4 +12,6 @@ router.register(r"movies", MovieViewSet, basename="movies")
 urlpatterns = [
     path("", ListMovie.as_view()),
     path("<int:pk>/", DetailMovie.as_view()),
+    path('movies/', MovieListView.as_view()),
+    path('movies/<int:pk>/', MovieDetailView.as_view()),
 ]
--- movie_review/movie_api/views.py
+++ movie_review/movie_api/views.py
@@ -4,6 +4,10 @@ from movies import models
 from .serializers import moviesSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from movies.models import movies as Movie
 # Create your views here.
 
 class ListMovie(generics.ListCreateAPIView):
@@ -22,6 +26,53 @@ class DetailMovie(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = moviesSerializer
     permission_classes = [IsAdminUser]
 
+    def post(self, request):
+        # Only admin users can create new model instances
+        serializer = moviesSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = models.movies.objects.all()
     serializer_class = moviesSerializer
+
+
+#API END POINTS BELOW
+class MovieListView(APIView):
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = moviesSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = moviesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MovieDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        movie = self.get_object(pk)
+        serializer = moviesSerializer(movie)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        movie = self.get_object(pk)
+        serializer = moviesSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        movie = self.get_object(pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

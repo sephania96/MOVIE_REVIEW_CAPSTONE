3ea03464b49d9d396f0bd57f8fae74a667f5f051
Sephania96 <koutanyi@yahoo.com>
2024-10-08 20:40:35 +0100

    deleting apps to start over again

--- movie_review/movie_api/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-from rest_framework.permissions import BasePermission
-
-class IsAdminOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
-            return True
-        return request.user.is_staff

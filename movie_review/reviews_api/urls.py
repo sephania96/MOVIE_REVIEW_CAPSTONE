8590f5957369ddfcfe248239c8fbab31f9ff9f3a
Sephania96 <koutanyi@yahoo.com>
2024-10-09 13:23:56 +0100

    root api endpoint added

--- movie_review/reviews_api/urls.py
+++ movie_review/reviews_api/urls.py
@@ -5,10 +5,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path("movies/", views.MovieList.as_view()),
     path("movies/<int:pk>/", views.MovieDetail.as_view()),
-    path("reviews/", views.ReviewList.as_view()),
+    path("reviews/", views.ReviewList.as_view(),name="reviews-list"),
     path("reviews/<int:pk>/", views.ReviewDetail.as_view()),
-    path("users/", views.UserList.as_view()),
+    path("users/", views.UserList.as_view(),name="user-list"),
     path("users/<int:pk>/", views.UserDetail.as_view()),
+    path("", views.api_root),
 ]
 
 #This is to allow the API to accept different formats
--- movie_review/reviews_api/views.py
+++ movie_review/reviews_api/views.py
@@ -2,38 +2,58 @@ from django.shortcuts import render
 from rest_framework import generics,permissions
 from .models import Movie, Review, CustomUser
 from .serializers import MovieSerializer, ReviewSerializer, CustomUserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.decorators import api_view  # new
+from rest_framework.response import Response  # new
+from rest_framework.reverse import reverse  # new
+
 # Create your views here.
 
 class MovieList(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = [IsAuthenticatedOrReadOnly]  # new
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class ReviewList(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes =[IsAuthenticated]
 
 
 class UserList(generics.ListAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+#single root API Endpoint
+@api_view(["GET"]) 
+def api_root(request, format=None):
+    return Response(
+        {
+            "users": reverse("user-list", request=request, format=format),
+            "reviews": reverse("reviews-list", request=request, format=format),
+        }
+    )

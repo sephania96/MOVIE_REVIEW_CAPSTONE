953f75c87b68b5cbb88d115c893cb605ff3b8638
Sephania96 <koutanyi@yahoo.com>
2024-10-09 11:48:00 +0100

    implementing authentication

--- movie_review/reviews_api/views.py
+++ movie_review/reviews_api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,permissions
 from .models import Movie, Review, CustomUser
 from .serializers import MovieSerializer, ReviewSerializer, CustomUserSerializer
 # Create your views here.
@@ -7,11 +7,13 @@ from .serializers import MovieSerializer, ReviewSerializer, CustomUserSerializer
 class MovieList(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class ReviewList(generics.ListCreateAPIView):
@@ -22,13 +24,16 @@ class ReviewList(generics.ListCreateAPIView):
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class UserList(generics.ListAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)

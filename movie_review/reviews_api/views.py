2dc15c33f0c9a94fe094353608f72106f8e07085
Sephania96 <koutanyi@yahoo.com>
2024-10-09 08:52:26 +0100

    adding my movie serializer to views

--- movie_review/reviews_api/views.py
+++ movie_review/reviews_api/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Movie, Review, User
+from .serializers import MovieSerializer
 # Create your views here.
 
+class movieList(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
 
+
+class movieDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
 

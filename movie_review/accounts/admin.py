11b9984593468ff05824cbef17c83f4639e11c49
Sephania96 <koutanyi@yahoo.com>
2024-10-18 14:39:15 +0100

    update

--- movie_review/accounts/admin.py
+++ movie_review/accounts/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+# Register your models here.
+
+
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
